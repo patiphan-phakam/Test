2593fafdcfdf37df3f2bade34a2c704283a07fb7
Pe-Patiphan-Phakam <patiphanp@osd.co.th>
2023-10-02 23:03:05 +0700

    Update link

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 export const config = {
   //   urlBackEnd: process.env.urlBacked || "http://localhost:3002/api",
-  backendUrl: "http://localhost:3002/api",
+  backendUrl: "http://buy-sri.com/service/api",
+  // backendUrl: "http://localhost:3002/api",
 };
